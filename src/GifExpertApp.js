0d826f73ea9f2298d83046620786eef7bc1cf676
Edgard Vilo <evilo@hostyweb.cl>
2021-11-20 16:53:55 -0300

    clase 70

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import { useState } from 'react';
 
 const GifExpertApp = () => {
+
+    // const categories = ['One punch', 'Luffy', 'Naruto'];
+    const [categories, setCategories] = useState(['One punch', 'Luffy', 'Naruto']);
+
+    const handleAdd = () => {
+        // setCategories([...categories, 'Hunterx']);
+        setCategories(cats => [...cats, 'Hunterx']);
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category =>
+                        <li key={category}>{category}</li>
+                    )
+                }
+            </ol>
         </>
     )
 }
