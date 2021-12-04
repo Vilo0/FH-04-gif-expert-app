c562e0569ecd655f6b4e3fc2a1190d186613f4d3
Edgard Vilo <evilo@hostyweb.cl>
2021-12-04 16:33:17 -0300

    clase 74

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    }, []);
 
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick&limit=10&api_key=Ult6O0L8EpsXouxqN8xgxI6hyOsLgVjf';
@@ -18,11 +24,11 @@ const GifGrid = ({ category }) => {
         console.log(gifs);
     }
 
-    getGifs();
-
     return (
         <div>
             <h3>{ category }</h3>
+            <h3>{ count }</h3>
+            <button onClick={ () => setCount(count + 1) }></button>
         </div>
     )
 }
