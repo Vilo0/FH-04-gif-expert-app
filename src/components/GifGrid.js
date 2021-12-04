9b4ded32c89c89f33a8ea21384ffbedc44d875b6
Edgard Vilo <evilo@hostyweb.cl>
2021-12-04 20:30:43 -0300

    clase 78

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
-import GifGridItem from './GifGridItem';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+// import { getGifs } from '../helpers/getGifs';
+// import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
+    const { loading } = useFetchGifs();
 
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category]);
+    // useEffect(() => {
+    //     getGifs(category).then(setImages);
+    // }, [category]);
 
     return (
         <>
             <h3>{ category }</h3>
-            <div className='card-grid'>
+            { loading ? 'Cargando' : 'Data Cargada' }
+            {/* <div className='card-grid'>
                 {
                     images.map( image => (
                         <GifGridItem key={image.id} { ...image } />
                     ))
                 }
-            </div>
+            </div> */}
         </>
     )
 }
