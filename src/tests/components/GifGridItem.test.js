05aef92eacebbbbce6c4d3294e4f0e79642e4b2a
Edgard Vilo <thevilos@gmail.com>
2021-12-05 11:12:23 -0300

    clase 89

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,7 +3,18 @@ import GifGridItem from "../../components/GifGridItem";
 
 describe('Pruebas en <GifGridItem />', () => {
     test('debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem /> );
+        const title = 'Un título';
+        const url = 'https://localhost';
+        const wrapper = shallow( <GifGridItem title={title} url={url} /> );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el componente correctamente simplificado', () => {
+        const props = {
+            title: 'Un título',
+            url: 'https://localhost'
+        };
+        const wrapper = shallow( <GifGridItem {...props} /> );
         expect(wrapper).toMatchSnapshot();
     });
 })
