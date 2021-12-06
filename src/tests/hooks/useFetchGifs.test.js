c820ca24819a27e775a2e0c8044aa0a99e268f8e
Edgard Vilo <thevilos@gmail.com>
2021-12-05 21:54:05 -0300

    clase 99

---
+++ src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,13 @@
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from "@testing-library/react-hooks";
+
+describe('Pruebas en el hook useFetchGifs', () => {
+    test('debe de retornar el estado inicial', () => {
+        const { result } = renderHook( () => useFetchGifs('Luffy') );
+        const { data, loading } = result.current;
+
+       expect( data ).toEqual([]);
+       expect( loading ).toBe(true);
+    })
+    
+})
